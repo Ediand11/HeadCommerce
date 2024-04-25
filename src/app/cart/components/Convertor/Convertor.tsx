7f07758507e99f20f576f459dbb729847e65ebe8
“Ediand11” <ediand11@yandex.ru>
2024-04-25 17:02:13 +0600

    fix: convertor component

--- src/app/cart/components/Convertor/Convertor.tsx
+++ src/app/cart/components/Convertor/Convertor.tsx
@@ -6,29 +6,35 @@ import { useState } from "react";
 import { Button } from "../../../../components/Button";
 import style from "./Convertor.module.scss";
 
-const Coupon = () => {
-  const [convert, setConvert] = useState<number>(0);
+const Convertor = () => {
+  const [convert, setConvert] = useState<string>("");
   const dispatch = useAppDispatch();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = event.target.value;
-    const convertedValue = parseInt(inputValue, 10);
+    setConvert(inputValue);
+  };
 
-    setConvert(convertedValue);
+  const handleSubmit = () => {
+    const convertedValue = parseInt(convert, 10);
+    if (!isNaN(convertedValue)) {
+      dispatch(addCoin(convertedValue));
+      setConvert("");
+    }
   };
 
   return (
     <>
       <h2 className={style.title}>Convertor Dollar to Coin</h2>
       <div className={style.form}>
-        <input className={style.input} type="number" value={convert} onChange={handleChange} />
-        <Button
-          className={style.button}
-          onClick={() => {
-            dispatch(addCoin(convert));
-            setConvert(0);
-          }}
-        >
+        <input
+          className={style.input}
+          placeholder="1000"
+          type="number"
+          value={convert}
+          onChange={handleChange}
+        />
+        <Button className={style.button} onClick={handleSubmit}>
           Apply
         </Button>
       </div>
@@ -36,4 +42,4 @@ const Coupon = () => {
   );
 };
 
-export default Coupon;
+export default Convertor;
